97142cbcacb3fc84862af3305f2f5253693fc62a
Andriy <andriy.zymenko@gmail.com>
2016-10-09 23:07:37 -0700

    5001 port

--- src/MediaLibrary/Program.cs
+++ src/MediaLibrary/Program.cs
@@ -17,6 +17,7 @@ namespace MediaLibrary
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+		.UseUrls("http://localhost:5001/")
                 .Build();
 
             host.Run();
