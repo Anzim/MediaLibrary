f8b6e0580dedf4c7c1f86abe6069b25ef21513b4
Andriy <andriy.zymenko@gmail.com>
2016-10-10 16:16:45 +0300

    Ready for testing

--- src/MediaLibrary/Controllers/FoldersController.cs
+++ src/MediaLibrary/Controllers/FoldersController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using MediaLibrary.Models;
+using MediaLibrary.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,45 +17,91 @@ namespace MediaLibrary.Controllers
         protected IQueryable<Folder> Folders { get; set; }
         public FoldersController(MediaLibraryContext context)
         {
+            context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
             Context = context;
-            Folders = Context.Folders;
+            Folders = Context.Folders.AsNoTracking();
         }
 
         
 
         // GET api/folders
         [HttpGet]
-        public IEnumerable<Folder> Get()
+        public async Task<IList<FolderInfo>> GetList()
         {
-            return Folders.Where(f => f.ParentId == 0).ToList();
+            var result = await Folders
+                .Where(f => f.ParentId == 0)
+                .OrderBy(f => f.FolderName)
+                .Select(f => new FolderInfo { FolderId = f.FolderId, FolderName = f.FolderName, IsCategory = f.IsCategory})
+                .ToListAsync();
+
+            return result;
+        }
+
+        // GET api/folders/details
+        [HttpGet("details")]
+        public async Task<IList<FolderDetails>> GetListWithDetails()
+        {
+            var result = await Folders
+                .Where(f => f.ParentId == 0)
+                .OrderBy(f => f.FolderName)
+                .ToListAsync();
+
+            return result.Select(f =>
+            {
+                var r = new FolderDetails();
+                r.CopyFrom(f);
+                return r;
+            }).ToList();
         }
 
         // GET api/folders/5
         [HttpGet("{id}")]
-        public Folder Get(int id)
+        public async Task<FolderDetails> GetDetails(int id)
         {
-            return Context.Folders.OrderBy(f => f.FolderName).Include(f => f.Children).FirstOrDefault(f => f.FolderId == id);
+            var result = await Folders
+                .FirstOrDefaultAsync(f => f.FolderId == id);
+
+            return result;
+        }
+
+        // GET api/folders/5/children
+        [HttpGet("{id}/children")]
+        public async Task<IList<FolderInfo>> GetChildren(int id)
+        {
+            var result = await Folders
+                .Where(f => f.ParentId == id)
+                .Select(ci => new FolderInfo { FolderId = ci.FolderId, FolderName = ci.FolderName, IsCategory = ci.IsCategory } )
+                .OrderBy(f => f.FolderName)
+                .ToListAsync();
+            
+            return result;
+            //var childrenInfo = await Folders
+            //    .OrderBy(f => f.FolderName)
+            //    .Select(f => new ForderInfo { FolderId = f.FolderId, FolderName = f.FolderName, IsCategory = f.IsCategory })
+            //    .ToListAsync();
+
+            //return Folders.OrderBy(f => f.FolderName).Include(f => new f.Children).FirstOrDefault(f => f.FolderId == id);
         }
 
         // POST api/folders
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]string value)
         {
-            throw new NotImplementedException();
+            return new StatusCodeResult(501);
         }
 
         // PUT api/folders/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]string value)
         {
-            throw new NotImplementedException();
+            return new StatusCodeResult(501);
         }
 
         // DELETE api/folders/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
-            throw new NotImplementedException();
+            return new StatusCodeResult(501);
         }
     }
 }
---
+++ src/MediaLibrary/ViewModels/Asset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace MediaLibrary.ViewModels
+{
+    public class Asset
+    {
+        public void CopyFrom<T>(T source)
+        {
+            var type = typeof(T);
+            IEnumerable<string> propertyNames = type.GetProperties().Select(p => p.Name);
+            CopyFrom(source, propertyNames);
+        }
+
+        public void CopyFrom<T>(T source, IEnumerable<string> propertyNames)//, bool ignoreErrors = true)
+        {
+            var sourceType = typeof(T);
+            var type = this.GetType();
+            foreach (var sourcePropertyName in propertyNames)
+            {
+                var targetProperty = type.GetProperty(sourcePropertyName);
+                targetProperty?.SetValue(this, sourceType.GetProperty(sourcePropertyName).GetValue(source, null), null);
+            }
+            //foreach (var sourceField in type.GetFields())
+        }
+    }
+}
